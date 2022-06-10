f91df653cefded2cf6e040e4d7801341022c937d
Jesse Johnston <jesse.johnston@gmail.com>
2022-06-09 21:12:56 -0700

    Add author

--- combustion-android-ble/src/main/java/inc/combustion/framework/ble/uart/SetColorRequest.kt
+++ combustion-android-ble/src/main/java/inc/combustion/framework/ble/uart/SetColorRequest.kt
@@ -1,7 +1,7 @@
 /*
  * Project: Combustion Inc. Android Framework
  * File: SetColor.kt
- * Author:
+ * Author: https://github.com/jjohnstz
  *
  * MIT License
  *
--- combustion-android-ble/src/main/java/inc/combustion/framework/ble/uart/SetIDRequest.kt
+++ combustion-android-ble/src/main/java/inc/combustion/framework/ble/uart/SetIDRequest.kt
@@ -1,7 +1,7 @@
 /*
  * Project: Combustion Inc. Android Framework
  * File: SetID.kt
- * Author:
+ * Author: https://github.com/jjohnstz
  *
  * MIT License
  *
