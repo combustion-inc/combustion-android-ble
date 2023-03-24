7f164ffd4363d91d9405c0053775ed0074772f7c
martin williams <martin@sleepygorilla.com>
2023-03-23 22:12:15 -0700

    Added Read Firmware Rev, Hardware Rev, and Session Info Message req/resp

---
+++ combustion-android-ble/src/main/java/inc/combustion/framework/ble/uart/meatnet/NodeReadSessionInfoResponse.kt
@@ -0,0 +1,67 @@
+/*
+ * Project: Combustion Inc. Android Framework
+ * File: NodeReadSessionInfoResponse.kt
+ * Author:
+ *
+ * MIT License
+ *
+ * Copyright (c) 2023. Combustion Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package inc.combustion.framework.ble.uart.meatnet
+
+import inc.combustion.framework.ble.getLittleEndianUInt16At
+import inc.combustion.framework.ble.getLittleEndianUInt32At
+import inc.combustion.framework.service.SessionInformation
+
+internal class NodeReadSessionInfoResponse (
+    sessionInformation: SessionInformation,
+    success: Boolean,
+    requestId: UInt,
+    responseId: UInt,
+    payloadLength: UByte,
+) : NodeResponse(success, requestId, responseId, payloadLength) {
+
+    companion object {
+
+        /// payload length 10 = session id (4) + sample period (4) +
+        const val PAYLOAD_LENGTH: UByte = 10u
+
+        fun fromData(
+            payload: UByteArray,
+            success: Boolean,
+            requestId: UInt,
+            responseId: UInt,
+            payloadLength: UByte
+        ) : NodeReadSessionInfoResponse? {
+            if (payloadLength < PAYLOAD_LENGTH) {
+                return null
+            }
+
+            val serialNumber: UInt = payload.getLittleEndianUInt32At(HEADER_SIZE.toInt())
+            val sessionID: UInt = payload.getLittleEndianUInt32At(HEADER_SIZE.toInt() + 4)
+            val samplePeriod: UInt = payload.getLittleEndianUInt16At(HEADER_SIZE.toInt() + 8)
+            val sessionInfo = SessionInformation(sessionID, samplePeriod)
+
+            return NodeReadSessionInfoResponse(sessionInfo, success, requestId, responseId, payloadLength)
+        }
+    }
+}
