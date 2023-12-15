80bf497f1f91f06f6ea473d519169170676652d5
Nick Helseth <nick@sasq.io>
2023-12-15 14:51:27 -0800

    DROID-311: settings.gradle: migrate to kotlin DSL

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,9 +1,12 @@
+import java.net.URI
+
+@Suppress("UnstableApiUsage")
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
-        maven { url "https://www.jitpack.io" }
+        maven { url = URI("https://www.jitpack.io") }
     }
 }
 rootProject.name = "Combustion Inc. Android Framework"
