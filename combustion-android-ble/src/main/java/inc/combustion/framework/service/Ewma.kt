27a5ef7e99e0a1bfe67b7e5c4a73e364561f305f
Nick Helseth <nick@sasq.io>
2024-01-06 06:12:54 -0800

    DROID-281: Add support for proximity identification and smoothed RSSI

---
+++ combustion-android-ble/src/main/java/inc/combustion/framework/service/Ewma.kt
@@ -0,0 +1,53 @@
+/*
+ * Project: Combustion Inc. Android Framework
+ * File: Ewma.kt
+ * Author: Nick Helseth <nick@combustion.inc>
+ *
+ * MIT License
+ *
+ * Copyright (c) 2023. Combustion Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package inc.combustion.framework.service
+
+internal class Ewma(
+    span: UInt,
+) {
+    private var alpha = if (span == 0u) 1.0 else 2.0 / (span.toFloat() + 1.0)
+    private var currentValue: Double? = null
+
+    val value: Double?
+        get() = currentValue
+
+    fun put(value: Double) {
+        currentValue = currentValue?.let {
+            alpha * value + (1.0 - alpha) * it
+        } ?: value
+    }
+
+    fun set(value: Double) {
+        currentValue = value
+    }
+
+    fun reset() {
+        currentValue = null
+    }
+}
