425de66caead52cffaf18781a9216b979a2e4556
Nick Helseth <nick@sasq.io>
2024-06-12 17:39:03 -0700

    Format PredictionManager

--- combustion-android-ble/src/main/java/inc/combustion/framework/service/utils/PredictionManager.kt
+++ combustion-android-ble/src/main/java/inc/combustion/framework/service/utils/PredictionManager.kt
@@ -41,11 +41,10 @@ import java.util.*
  * updates (i.e., updating due to an incoming status message) should set this to false to indicate
  * that the status is being updated elsewhere.
  */
-typealias CompletionHandler =
-    (predictionInfo: PredictionManager.PredictionInfo?, shouldUpdateStatus: Boolean) -> Unit
+typealias CompletionHandler = (predictionInfo: PredictionManager.PredictionInfo?, shouldUpdateStatus: Boolean) -> Unit
 
 class PredictionManager {
-    data class PredictionInfo (
+    data class PredictionInfo(
         val predictionState: ProbePredictionState,
         val predictionMode: ProbePredictionMode,
         val predictionType: ProbePredictionType,
@@ -62,23 +61,23 @@ class PredictionManager {
         private const val PREDICTION_STALE_TIMEOUT = 15000L // 15 seconds in ms
 
         /// Cap the prediction to 6 hours
-        private const val MAX_PREDICTION_TIME : UInt = 21600u // 6 hours
+        private const val MAX_PREDICTION_TIME: UInt = 21600u // 6 hours
 
         /// Number of samples to wait between updates to prediction 'seconds remaining',
         /// for syncing time remaining across apps, Displays etc.
-        private const val PREDICTION_TIME_UPDATE_COUNT : UInt = 3u
+        private const val PREDICTION_TIME_UPDATE_COUNT: UInt = 3u
 
         /// Number of prediction seconds at or above which to round the 'time remaining' display.
-        private const val LOW_RESOLUTION_CUTOFF_SECONDS : UInt = 300u // 5 minutes
+        private const val LOW_RESOLUTION_CUTOFF_SECONDS: UInt = 300u // 5 minutes
 
         /// Resolution of prediction display in 'low resolution' mode.
-        private const val LOW_RESOLUTION_PRECISION_SECONDS : UInt = 15u
+        private const val LOW_RESOLUTION_PRECISION_SECONDS: UInt = 15u
 
         /// Rate at which the linearization timer is run
-        private const val LINEARIZATION_UPDATE_RATE_MS : Double = 200.0
+        private const val LINEARIZATION_UPDATE_RATE_MS: Double = 200.0
 
         /// Rate at which the prediction status information is transmitted
-        private const val PREDICTION_STATUS_RATE_MS : Double = 5000.0
+        private const val PREDICTION_STATUS_RATE_MS: Double = 5000.0
     }
 
     private var previousPredictionInfo: PredictionInfo? = null
@@ -118,8 +117,7 @@ class PredictionManager {
         // Duplicate status messages are sent when prediction is started. Ignore the duplicate sequence number
         // unless the prediction information has changed
         previousSequenceNumber?.let {
-            if (it == sequenceNumber &&
-                predictionStatus?.setPointTemperature == previousPredictionInfo?.predictionSetPointTemperature) {
+            if (it == sequenceNumber && predictionStatus?.setPointTemperature == previousPredictionInfo?.predictionSetPointTemperature) {
                 return null
             }
         }
@@ -139,13 +137,16 @@ class PredictionManager {
         return predictionInfo
     }
 
-    private fun infoFromStatus(predictionStatus: PredictionStatus?, sequenceNumber: UInt): PredictionInfo? {
+    private fun infoFromStatus(
+        predictionStatus: PredictionStatus?,
+        sequenceNumber: UInt
+    ): PredictionInfo? {
         val status = predictionStatus ?: return null
 
         val secondsRemaining = secondsRemaining(status, sequenceNumber)
 
         return PredictionInfo(
-            predictionState =status.predictionState,
+            predictionState = status.predictionState,
             predictionMode = status.predictionMode,
             predictionType = status.predictionType,
             predictionSetPointTemperature = status.setPointTemperature,
@@ -160,14 +161,18 @@ class PredictionManager {
 
     private fun secondsRemaining(predictionStatus: PredictionStatus, sequenceNumber: UInt): UInt? {
         // Do not return a value if not in predicting state
-        if (predictionStatus.predictionState != ProbePredictionState.PREDICTING) { return null }
+        if (predictionStatus.predictionState != ProbePredictionState.PREDICTING) {
+            return null
+        }
 
         // Do not return a value if above max seconds remaining
-        if (predictionStatus.predictionValueSeconds > MAX_PREDICTION_TIME) { return null }
+        if (predictionStatus.predictionValueSeconds > MAX_PREDICTION_TIME) {
+            return null
+        }
 
         val previousSecondsRemaining = previousPredictionInfo?.secondsRemaining
 
-        if(predictionStatus.predictionValueSeconds > LOW_RESOLUTION_CUTOFF_SECONDS) {
+        if (predictionStatus.predictionValueSeconds > LOW_RESOLUTION_CUTOFF_SECONDS) {
 
             runningLinearization = false
 
@@ -175,16 +180,16 @@ class PredictionManager {
             // (unless we don't yet have a value), using modulo to sync with other apps, Displays etc.
             if (previousSecondsRemaining == null || sequenceNumber % PREDICTION_TIME_UPDATE_COUNT == 0u) {
                 // In low-resolution mode, round the value to the nearest 15 seconds.
-                val remainder = predictionStatus.predictionValueSeconds % LOW_RESOLUTION_PRECISION_SECONDS
-                if(remainder > (LOW_RESOLUTION_PRECISION_SECONDS / 2u)) {
+                val remainder =
+                    predictionStatus.predictionValueSeconds % LOW_RESOLUTION_PRECISION_SECONDS
+                if (remainder > (LOW_RESOLUTION_PRECISION_SECONDS / 2u)) {
                     // Round up
                     return predictionStatus.predictionValueSeconds + (LOW_RESOLUTION_PRECISION_SECONDS - remainder)
                 } else {
                     // Round down
                     return predictionStatus.predictionValueSeconds - remainder
                 }
-            }
-            else {
+            } else {
                 return previousSecondsRemaining
             }
         } else {
@@ -195,21 +200,21 @@ class PredictionManager {
             // the linearization should hit the current prediction - 5 seconds
             val predictionUpdateRateSeconds = (PREDICTION_STATUS_RATE_MS / 1000.0).toUInt()
 
-            if(predictionStatus.predictionValueSeconds < predictionUpdateRateSeconds) {
+            if (predictionStatus.predictionValueSeconds < predictionUpdateRateSeconds) {
                 linearizationTargetSeconds = 0
-            }
-            else {
-                linearizationTargetSeconds = (predictionStatus.predictionValueSeconds - predictionUpdateRateSeconds).toInt()
+            } else {
+                linearizationTargetSeconds =
+                    (predictionStatus.predictionValueSeconds - predictionUpdateRateSeconds).toInt()
             }
 
             if (!runningLinearization) {
                 // If not already running linearization, then initialize values
                 currentLinearizationMs = predictionStatus.predictionValueSeconds.toDouble() * 1000.0
                 linearizationTimerUpdateValue = LINEARIZATION_UPDATE_RATE_MS
-            }
-            else {
+            } else {
                 val intervalCount = PREDICTION_STATUS_RATE_MS / LINEARIZATION_UPDATE_RATE_MS
-                linearizationTimerUpdateValue = (currentLinearizationMs - linearizationTargetSeconds * 1000.0) / intervalCount
+                linearizationTimerUpdateValue =
+                    (currentLinearizationMs - linearizationTargetSeconds * 1000.0) / intervalCount
             }
 
             // Create a new linearization timer
@@ -219,7 +224,7 @@ class PredictionManager {
 
             // Start the linearization timer
             val intervalMs = LINEARIZATION_UPDATE_RATE_MS.toLong()
-            linearizationTimer?.schedule( object : TimerTask() {
+            linearizationTimer?.schedule(object : TimerTask() {
                 override fun run() {
                     updatePredictionSeconds()
                 }
@@ -232,12 +237,12 @@ class PredictionManager {
     }
 
     private fun updatePredictionSeconds() {
-        val previousInfo = previousPredictionInfo  ?: return
+        val previousInfo = previousPredictionInfo ?: return
 
         currentLinearizationMs -= linearizationTimerUpdateValue
 
         // Don't let the linearization value go below 0 or the UInt conversion will crash.
-        if(currentLinearizationMs < 0.0) {
+        if (currentLinearizationMs < 0.0) {
             currentLinearizationMs = 0.0
         }
 
@@ -259,7 +264,7 @@ class PredictionManager {
         publishPredictionInfo(predictionInfo = predictionInfo, shouldUpdateStatus = true)
 
         // Stop the timer if the prediction has gone stale
-        if( (SystemClock.elapsedRealtime() - linearizationStartTime) >= PREDICTION_STALE_TIMEOUT ) {
+        if ((SystemClock.elapsedRealtime() - linearizationStartTime) >= PREDICTION_STALE_TIMEOUT) {
             clearLinearizationTimer()
         }
     }
@@ -270,17 +275,17 @@ class PredictionManager {
         val core = predictionStatus.estimatedCoreTemperature
 
         // Max percentage is 100
-        if(core > end) {
+        if (core > end) {
             return 100
         }
 
         // Minimum percentage is 0
-        if(start > core) {
+        if (start > core) {
             return 0
         }
 
         // This should never happen, but would cause a crash
-        if(end == start) {
+        if (end == start) {
             return 100
         }
 
